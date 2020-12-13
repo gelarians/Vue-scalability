04b7322b440839c6276111a72467809ec4a480ad
Dave <info@new-fantasy.eu>
2020-12-13 11:50:24 +0100

    add basic components

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <p class="lead">Hallo aus der HomePage.vue :)</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style>
+</style>
